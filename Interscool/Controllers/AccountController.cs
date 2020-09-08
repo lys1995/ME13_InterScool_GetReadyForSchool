d6746f1245719d11203a3cdd72e00ca4448ded88
Yanshi Liu <yliu0203@student.monash.edu>
2020-09-08 19:44:20 +1000

    Add password protection

---
+++ Interscool/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Interscool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Interscool.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IAuthenticationManager Authentication => HttpContext.GetOwinContext().Authentication;
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous, HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            //TODO Perform Validation
+            //Sign User in
+            if (model.Password == "ME13")
+            {
+                SignIn(model);
+                //Redirect to Home
+                return RedirectToAction("index", "home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login failed. Please double check password");
+                return View(model);
+            }
+        }
+
+        private void SignIn(LoginModel model)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,"1")
+            };
+            var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+            Authentication.SignIn(identity);
+        }
+    }
+}
---
+++ Interscool/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Interscool.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="The Password can not be empty")]
+        public string Password { get; set; }
+    }
+}
